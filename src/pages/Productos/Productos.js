bf6b0aec4eee286de08f2697e14bc49df716b69a
Neri Armijo Miranda <neriarmijomiranda530@gmail.com>
2024-07-07 18:34:03 -0600

    Estructura de productos, prueba

--- src/pages/Productos/Productos.js
+++ src/pages/Productos/Productos.js
@@ -3,9 +3,8 @@
 import '/style.scss'
 import * as bootstrap from 'bootstrap' /* Para desplegar el menú / activar cosas de bootstrap */
 import './Productos.css'
-import '/src/components/footer/footer.css'
-import '/src/components/navbar/navbar.css'
-
+import '/Proyecto-Dinamita-Java-Queens/src/components/footer/footer.css'
+import '/src/components/footer\footer-app.js'
 import { navbarApp } from '/src/components/navbar/navbar-app.js'
 import { footerApp } from '/src/components/footer/footer-app.js'
 

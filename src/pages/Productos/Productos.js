b648a77ad52699b1dd988fec8ef038267c92ba7a
Marbella Cabrera <marbecduran@gmail.com>
2024-07-08 21:06:11 -0600

    Acrualizacion de los nombres de las ultimas paginas que agregaron

--- src/pages/Productos/Productos.js
+++ src/pages/Productos/Productos.js
@@ -13,8 +13,8 @@ import { footerApp } from '/src/components/footer/footer_app.js'
 import './Productos.css'
 import '/Proyecto-Dinamita-Java-Queens/src/components/footer/footer.css'
 import '/src/components/footer\footer-app.js'
-import { navbarApp } from '/src/components/navbar/navbar-app.js'
-import { footerApp } from '/src/components/footer/footer-app.js'
+import { navbarApp } from '/src/components/navbar/navbar_app.js'
+import { footerApp } from '/src/components/footer/footer_app.js'
 >>>>>>> Neri
 
 

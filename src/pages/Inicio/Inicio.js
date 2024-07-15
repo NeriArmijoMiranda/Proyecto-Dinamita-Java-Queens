2435f2a3a982903f5d9c5e52686c8804969db54c
Marbella Cabrera <marbecduran@gmail.com>
2024-07-15 17:47:49 -0600

    Agregue modificaciones a la pagina de inicio, en el carrusel

--- src/pages/Inicio/Inicio.js
+++ src/pages/Inicio/Inicio.js
@@ -14,3 +14,31 @@ document.querySelector("#navbar-app").innerHTML= navbarApp();
 document.querySelector("#footer-app").innerHTML= footerApp();
 
 /* ------------------------------------------------------------- */
+
+const track = document.querySelector('.carousel__track');
+const slides = Array.from(track.children);
+const slideWidth = slides[0].getBoundingClientRect().width;
+let visibleSlides = 3;
+let currentIndex = 0;
+
+function moveSlide(direction) {
+    const mediaQuery = window.matchMedia('(max-width: 768px)');
+    visibleSlides = mediaQuery.matches ? 1 : 3;
+    currentIndex += direction * visibleSlides;
+
+    if (currentIndex < 0) {
+        currentIndex = slides.length - visibleSlides;
+    } else if (currentIndex >= slides.length) {
+        currentIndex = 0;
+    }
+
+    const amountToMove = slideWidth * currentIndex;
+    track.style.transform = 'translateX(-' + amountToMove + 'px)';
+}
+
+// Update slideWidth and currentIndex on window resize
+window.addEventListener('resize', () => {
+    const currentSlide = document.querySelector('.carousel__slide');
+    slideWidth = currentSlide.getBoundingClientRect().width;
+    moveSlide(0);
+});

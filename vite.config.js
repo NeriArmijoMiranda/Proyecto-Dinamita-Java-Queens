d2f745538d47641d1546b3fdafa88392d4310bd4
Neri Armijo Miranda <neriarmijomiranda530@gmail.com>
2024-07-16 21:14:32 -0600

    pruebas1

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
             input:{
                 main: resolve(__dirname, 'index.html'),
                 acercaNosotros: resolve(__dirname,'src/pages/acerca_de_nosotros/acerca_de_nosotros.html'),
-                compras: resolve(__dirname,'src/pages/compras/Compras.html'),
+                compras: resolve(__dirname,'src/pages/compras/compras.html'),
                 contactanos: resolve (__dirname,'src/pages/contactanos/contactanos.html'),
                 datosEnvio: resolve (__dirname,'src/pages/datos_de_envio/datos_de_envio.html'),
                 formCreaObjetos: resolve (__dirname,'src/pages/Form_crea_objetos/form_crea_objetos.html'),

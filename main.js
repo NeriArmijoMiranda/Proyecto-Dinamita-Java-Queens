0719cc11cb6875f0701bec3afce261a7d4f31ca6
Desire Garcia <desiregrc@gmail.com>
2024-07-19 18:32:49 -0600

    Iniciar Sesión completa

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 /* ----NO BORRAR------------ */
 /* Aquí van las rutas de conexión */
 import '/style.scss' /* Estilos generales */
+import * as bootstrap from 'bootstrap' /* Para desplegar el menú / activar cosas de bootstrap */
 import '/src/components/footer/footer.css' /* Estilos del pie de pagina */
 import '/src/components/navbar/navbar.css' /* Estilo de la barra de navegacion */
-import { navbarApp } from '/src/components/navbar/navbar_app.js' /* funcionalidad de la barra de navegacion */
-import { footerApp } from '/src/components/footer/footer_app.js' /* funcionalidad del pie de pagina */
+import { navbarApp } from './src/components/navbar/navbar_app.js' /* funcionalidad de la barra de navegacion */
+import { footerApp } from './src/components/footer/footer_app.js' /* funcionalidad del pie de pagina */
 
 document.querySelector("#navbar-app").innerHTML= navbarApp(); /* insertar función navbarapp dentro del html */
 document.querySelector("#footer-app").innerHTML= footerApp();
+
+/* ----------------------------------------------- */

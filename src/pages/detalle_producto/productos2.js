edc4164dd70c65e63ca1947fc917c0f492d7d97a
Jhor Arellano <jhorleny.business@gmail.com>
2024-07-20 19:59:56 -0600

    se cambiaron detalles de páginas de productos en cards

--- src/pages/detalle_producto/productos2.js
+++ src/pages/detalle_producto/productos2.js
@@ -15,57 +15,101 @@ document.querySelector("#footer-app").innerHTML= footerApp();
 
 /* ------------------------------------------------------------------------------------------------------------ */
 
-// Espera a que el documento esté completamente cargado
-document.addEventListener("DOMContentLoaded", function() {
-    // Selecciona el carrusel por su ID
-    const myCarousel = document.getElementById('carouselExampleIndicators');
 
-    // Crea un objeto de carrusel de Bootstrap
-    const carousel = new bootstrap.Carousel(myCarousel, {
-        interval: 3000, // Intervalo de 3 segundos entre slides
-        wrap: true // Permite el ciclo infinito del carrusel
-    });
-});
-document.addEventListener("DOMContentLoaded", function() {
-    // Selecciona el botón de "Agregar al carrito"
-    const agregarCarritoBtn = document.getElementById('agregarCarrito');
+/*Las const son para llamarlas del html y decirles "Eh wey te estan hablando" */
+const shopContent = document.getElementById("shopContent");/*Esta es la primera variable de la parte número 1 la obtuvimos
+con el id que le pusimos en el HTML*/
+const categoryItems = document.querySelectorAll('.categoryitem');
+/* const verCarrito = document.getElementById("verCarrito");
+const modalContainer = document.getElementById("modal-container");
+const cantidadCarrito = document.getElementById("cantidadCarrito"); */
 
-    agregarCarritoBtn.addEventListener('click', function() {
-        // Obtén la cantidad seleccionada
-        const cantidad = document.getElementById('cantidad').value;
+/*Aquí puse la igualdad del carrito para que me lo recupere en el localstorage, al carrito se convierte en
+en lo que sea que este guardado en el localStorage. Aqui el carrito es básicamente, si hay algo guardado 
+se convierte en eso, pero si no hay nada pues está vacío */
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+
+/*Aqui hice unan funcion para llamar a los productos del Json y que corran por que si no les pegan */
+
+const getProducts = async () => {
+/*Ruta del archivo Json  */
+    const response = await fetch('/data.json');
+    const data = await response.json();
+    /*Aquí el data.forEach recorre todos los productos */
+    data.forEach((product) => {
+        let content = document.createElement("div");
+        /*Se puden agregar clases a los elementos HTML, como lo vimos en imagen por ejemplo */
+        content.className = "card";//Aquí por ejemplo esta la clase de card para el Css 
+        content.innerHTML = `
+        <center><img class="imagenProduct rounded-3" src= "${product.imagen}" margin-bottom="15px"></center>
+        <h2>${product.nombre}</h2>
+        <h3>${product.origen}</h3>
+        <h4>${product.categoría}</h4>
+        <div class="descripcion" align-items-center>
+        <p>Talla: ${product.talla}</p>
+        <p>${product.descripcion}</p>
+        <p>Precio: $${product.precio}</p>
+        <p>Cantidad: ${product.cantidad}</p>
+        </div>
+    `;
+    /*Con la propiedad append vamos a conectar la primera parte PARTE 1 */
+        shopContent.append(content);
         
-        // Obtén la talla seleccionada
-        const talla = document.getElementById('talla').value;
+//-------------------------------Boton detalles producto -----------------------------------------------------------
 
-        // Aquí puedes agregar lógica adicional, como enviar esta información a un carrito de compras
-        console.log('Cantidad:', cantidad);
-        console.log('Talla:', talla);
+
+    /*Aquí es donde pasa la magia, use varios métodos entre ellos push, map y some, está función de aquí;
+    lo que hace es que no se repitan los productos con todos sus atributos y solo se ponga la cantidad deseada,
+    es decir en vez de tener dos sombreros con su descripcioón, lugar de origen etc, solo se duplique la cantidad.
+    
+    Lo que hace .addEventListener es que cada que le des click a algún producto lo va a agregar como si fuera un timbre
+    llamando*/ 
+    
+        comprar.addEventListener("click", () => {
+    /*Aquí es lo que hace que no se repite el producto con todo, sino que solo duplique la cantidad*/ 
+            const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id); 
+    
+            if (repeat) {
+                carrito.map((prod) => {
+                    if(prod.id === product.id){
+                        prod.cantidad++;
+                        
+                    }
+                });
+            } else{
+    /*Aquí en el carrito es */
+            carrito.push({
+            id : product.id,
+            nombre: product.nombre,
+            precio: product.precio,
+            imagen: product.imagen,
+            origen: product.origen,
+            talla: product.talla,
+            cantidad: product.cantidad,
+            /* descripcion: product.descripcion, */
+            });  
+         
+        }
+        console.log(carrito);
+        console.log(carrito.length);
+        carritoCounter();
+    /*Aquí mismo pedí a la función que de favor me guardara lo de mi carrito */
+        saveLocal();
     });
-});
-document.addEventListener("DOMContentLoaded", function() {
-    // Obtiene todos los botones del acordeón
-    const accordionButtons = document.querySelectorAll('.accordion-button-detalles');
-
-    // Itera sobre cada botón
-    accordionButtons.forEach(function(button) {
-        // Agrega un evento de clic a cada botón
-        button.addEventListener('click', function() {
-            // Cierra todos los elementos colapsables que no sean el actual
-            accordionButtons.forEach(function(btn) {
-                if (btn !== button) {
-                    btn.classList.add('collapsed');
-                    btn.setAttribute('aria-expanded', 'false');
-                    const target = btn.getAttribute('data-bs-target');
-                    document.querySelector(target).classList.remove('show');
-                }
-            });
-        });
     });
-});
+};
 
+getProducts();
+export {carrito} /* Se exporta para tenerlo en página compras */
 
-
-
-
+//////////// Aqui quite algo, const saveLocal////////
+///////// aquí agregué algo del final del codigo de productos//
+/*Si te pierdes aquí va lo del carrito Mendoza del futuro*/
+/*El localStorage funciona cn set item get item */
+/*Primero es el set item, esto me la va a guardar*/
+const saveLocal = () => {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+};
 
 

a8589e20f72ac1812385d66199c2d18c18786a39
Desire Garcia <desiregrc@gmail.com>
2024-08-04 17:37:48 -0600

    cambios del background de los banner de diferentes secciones

--- src/pages/compras/compras.js
+++ src/pages/compras/compras.js
@@ -24,11 +24,7 @@ document.innerHTML = saveLocal();
 /*Las const son para llamarlas del html y decirles "Eh te estan hablando" */
 
 const contenedorGeneral = document.getElementById("contenedorGeneral"); /* Contenedor general de toda la página */
-<<<<<<< HEAD
-const modalContainer  = document.getElementById("modalContainer");/* descripción producto en html  */
-=======
 const Container  = document.getElementById("modalContainer");/* descripción producto en html  */
->>>>>>> Development
 const resumenCompras = document.getElementById("resumenCompras"); /* Resumen de compras html */
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 const verCarrito = document.getElementById("verCarrito");
@@ -155,13 +151,10 @@ const pintarCarrito = () => {
     continuar.innerText = "Continuar con la compra";/*Con innertext lo ponemos texto al botón  */
     continuar.className = "continuarBoton"; /*Aquí lo conectamos con content y le estamos diciendo que a cada producto le agregue un botón de comprar */
     resumenCompras.append(continuar);
-<<<<<<< HEAD
-=======
     // Agregar el evento click para redirigir
     continuar.addEventListener("click", function () {
         window.location.href = "/src/pages/datos_de_envio/datos_de_envio.html"; 
     });
->>>>>>> Development
 };
 pintarCarrito();
 
